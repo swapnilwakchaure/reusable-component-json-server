7f7c209717593c1c5f00bd9ee23665cd8e4e62dc
swapnilwakchaure <swapnilwakchaure2411999@gmail.com>
2023-11-02 13:23:33 +0530

    add json data

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server"); // importing json-server library
+const cors = require("cors");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = 8080; // you can use any port number here; i chose to use 3001
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
